ddee36c7429d551072ae694cf99fe3fb0175b839
DicarProjects <cardona.devprojects@gmail.com>
2023-11-17 03:31:39 -0500

    Customers is done

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -10,7 +10,7 @@ export const AboutUs = () => {
             <div className="flex flex-col justify-center">
                 <h2 className="mb-10 text-2xl font-bold">About Us</h2>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque aliquid libero blanditiis eaque, consectetur ad maxime quae. Veritatis pariatur qui quo nulla aperiam numquam, inventore blanditiis quibusdam? Commodi, accusamus. Id!</p>
-                <a className="bg-red-500 hover:bg-red-600 text-white rounded-lg p-3 w-6/12 mx-auto md:mx-0 lg:w-3/12 md:mt-8" href="#about">
+                <a className="bg-red-500 hover:bg-red-600 text-white rounded-lg p-3 w-6/12 mx-auto md:mx-0 lg:w-3/12 mt-10 md:mt-8" href="#about">
                     {t('Read more')}
                     <i className="fa-solid fa-arrow-right ml-2"></i>
                 </a>
--- src/components/Customers.tsx
+++ src/components/Customers.tsx
@@ -1,5 +1,52 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import useScreenSize from "../hooks/useScreenSize";
+import { Pagination } from 'swiper/modules';
+import { useTranslation } from "react-i18next";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import "swiper/css";
+import company from '../assets/img/company.png';
+
 export const Customers = () => {
-  return (
-    <div>Customers</div>
-  )
+
+    const { width } = useScreenSize();
+
+    const { t } = useTranslation();
+
+    AOS.init();
+
+    return (
+        <section className="my-20">
+
+            <div 
+                data-aos="fade-down"
+                data-aos-easing="linear"
+                data-aos-duration="700" 
+                className="bg-red-500 rounded-lg w-6/12 mx-auto lg:w-2/12 mb-10"
+            >
+                <p
+                    className="text-white p-2 text-sm font-bold text-center md:text-lg"
+                >{t('Customers')}</p>
+            </div>
+
+            <Swiper 
+                slidesPerView={width > 768 ? 5 : 2}
+                spaceBetween={30}
+                pagination={{
+                clickable: true,
+                }}
+                modules={[Pagination]}
+                className="mySwiper"
+            >
+                <SwiperSlide><img src={company} className="w-32 h-32"/></SwiperSlide>
+                <SwiperSlide><img src={company} className="w-32 h-32"/></SwiperSlide>
+                <SwiperSlide><img src={company} className="w-32 h-32"/></SwiperSlide>
+                <SwiperSlide><img src={company} className="w-32 h-32"/></SwiperSlide>
+                <SwiperSlide><img src={company} className="w-32 h-32"/></SwiperSlide>
+                <SwiperSlide><img src={company} className="w-32 h-32"/></SwiperSlide>
+                <SwiperSlide><img src={company} className="w-32 h-32"/></SwiperSlide>
+                <SwiperSlide><img src={company} className="w-32 h-32"/></SwiperSlide>
+            </Swiper>    
+        </section>
+    )
 }
