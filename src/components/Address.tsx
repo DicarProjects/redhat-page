e7db01784e48a13a81bb3630dfefde85562e06e7
DicarProjects <cardona.devprojects@gmail.com>
2023-11-19 21:49:16 -0500

    Fix on contact page

--- src/components/Address.tsx
+++ src/components/Address.tsx
@@ -6,7 +6,7 @@ export const Address = () => {
   const { t } = useTranslation();
 
   return (
-    <section className="flex flex-col md:flex-row container mx-auto md:max-w-[700px] xl:max-w-[1200px]">
+    <section className="flex flex-col md:flex-row container mx-auto md:max-w-[700px] xl:max-w-[1200px] mt-10">
       <div className="flex flex-col flex-1 items-start gap-5 m-10 md:m-0 md:mt-10">
         <h2 className="mb-10 text-2xl font-bold">{t('LATAM')}</h2>
         <p className="text-gray-400"><span className="font-bold text-black">{t('Direction')}:</span> Calle 154 # 21 Sur Ibagué, Tolima</p>
