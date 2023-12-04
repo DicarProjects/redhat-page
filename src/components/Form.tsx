cde7ed20f2db8f73cc2a443ba5ed794eafe1f7d3
DicarProjects <cardona.devprojects@gmail.com>
2023-12-04 02:27:02 -0500

    Validation

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,9 @@
 // import { useState } from "react";
 import { useTranslation } from "react-i18next";
+import clienteAxios from "../api/axios";
 import { useForm } from "../hooks/useForm";
+import Swal from 'sweetalert2';
+import 'sweetalert2/src/sweetalert2.scss'
 import Contact from "../assets/img/form.jpg";
 
 export const Form = () => {
@@ -10,17 +13,48 @@ export const Form = () => {
       name: '',
       email:'',
       subject: '',
-      description: ''
+      comment: ''
     }
     
     const [formValues, handleInputChange] = useForm(initialForm);
 
-    const { name, email, subject, description } = formValues;
+    const { name, email, subject, comment } = formValues;
+
+    const hasProperty = (object) => {
+      const resp = Object.values(object).some(value => !value);
+      return resp;
+    }
     
-    const login = (e) => {
+    const login = async (e) => {
       e.preventDefault();
 
-      //Aqui poner endpoint
+      console.log(formValues)
+
+      if (!hasProperty(formValues)) {
+        const response = await clienteAxios.post('/api/form/create-new-from', formValues);
+  
+        if (response.status === 200) {
+          Swal.fire({
+            title: "Good job!",
+            text: "You message was sent!",
+            icon: "success"
+          });
+        } else {
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Try again"
+          });
+        }
+      } else {
+        Swal.fire({
+          title: 'Error!',
+          text: 'Complete the form, please',
+          icon: 'error'
+        });
+      }
+
+
     }
 
     return (
@@ -64,9 +98,9 @@ export const Form = () => {
             />
 
             <textarea
-                id="description"
-                name="description"
-                value={description}
+                id="comment"
+                name="comment"
+                value={comment}
                 onChange={handleInputChange}
                 className="bg-gray-100 w-full p-3 rounded text-sm md:col-start-1 md:col-end-13 md:h-24 resize-none"
                 placeholder={t('Description')}
