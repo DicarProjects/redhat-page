4294c858ec9e7871b71815f0fe32e9079b96d3c8
DicarProjects <cardona.devprojects@gmail.com>
2023-12-06 09:14:59 -0500

    Contact is done

--- src/components/Address.tsx
+++ src/components/Address.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from "react-i18next";
+import { Title } from "./Title";
 import contact from "../assets/img/contact.jpg";
 
 export const Address = () => {
@@ -6,15 +7,15 @@ export const Address = () => {
   const { t } = useTranslation();
 
   return (
-    <section className="flex flex-col md:flex-row container mx-auto md:max-w-[700px] xl:max-w-[1200px] mt-10">
-      <div className="flex flex-col flex-1 items-start gap-5 m-10 md:m-0 md:mt-10">
-        <h2 className="mb-10 text-xl md:text-2xl lg:text-3xl font-bold">{t('LATAM')}</h2>
+    <section className="flex flex-col md:flex-row container mx-auto md:max-w-[700px] xl:max-w-[1200px] mt-10 p-5">
+      <div className="flex flex-col flex-1 items-start justify-center gap-5 m-10 md:m-0 md:mt-10">
+        <Title text="LATAM" center={false}/>
         <p className="text-gray-700 text-md text-[16px] font-serif"><span className="font-bold text-black">{t('Direction')}:</span> Calle 154 # 21 Sur Ibagué, Tolima</p>
         <p className="text-gray-700 text-md text-[16px] font-serif"><span className="font-bold text-black">{t('Phone')}:</span> (+57) 312 2538120</p>
         <p className="text-gray-700 text-md text-[16px] font-serif"><span className="font-bold text-black">{t('Email')}:</span> contact@redhatitsolutions.com</p>
       </div>
       <div className="flex-1">
-        <img className="" alt="Contact Image" src={contact}/>
+        <img className="h-[650px] object-cover" alt="Contact Image" src={contact}/>
       </div>
     </section>
   )
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,6 +2,7 @@
 import { useTranslation } from "react-i18next";
 import axiosClient from "../api/axios";
 import { useForm } from "../hooks/useForm";
+import { Title } from "./Title";
 import Swal from 'sweetalert2';
 import 'sweetalert2/src/sweetalert2.scss'
 import Contact from "../assets/img/form.jpg";
@@ -59,62 +60,64 @@ export const Form = () => {
     }
 
     return (
-      <div className="flex flex-col-reverse gap-10 md:flex-row container mx-auto xl:max-w-[1200px] my-10">
+      <div className="flex flex-col-reverse gap-10 md:flex-row container mx-auto xl:max-w-[1200px] my-10 p-5">
         <div className="flex-1">
-          <img src={Contact} alt="Contact Image" />
+          <img className="h-[600px] object-cover" src={Contact} alt="Contact Image" />
         </div>
-        <form 
-          className="flex flex-1 flex-col gap-5 md:grid md:grid-cols-12 p-5 md:p-0"
-          onSubmit={login}
-        >
-          <h2 className="text-2xl font-bold md:col-start-1 md:col-end-13 border-b-2 border-red-500 w-6/12 pb-2 mb-2">{t('Contact Us')}</h2>
+        <div className="flex-1 flex flex-col justify-center md:p-10">
+          <Title text="Contact Us" center={false}/>
+          <form 
+            className="flex flex-1 flex-col gap-5 md:grid md:grid-cols-12 p-5 md:p-0"
+            onSubmit={login}
+          >
             <input
-                id="name"
-                name="name"
-                type="text"
-                value={name}
-                onChange={handleInputChange}
-                className="bg-gray-100 w-full p-3 rounded text-sm md:col-start-1 md:col-end-13 placeholder-gray-500 font-serif"
-                placeholder={t('Name')}
+              id="name"
+              name="name"
+              type="text"
+              value={name}
+              onChange={handleInputChange}
+              className="bg-gray-100 w-full p-3 rounded text-sm md:col-start-1 md:col-end-13 placeholder-gray-500 font-serif"
+              placeholder={t('Name')}
             />
 
             <input
-                id="email"
-                name="email"
-                type="email"
-                value={email}
-                onChange={handleInputChange}
-                className="bg-gray-100 w-full p-3 rounded text-sm md:col-start-1 md:col-end-13 placeholder-gray-500 font-serif"
-                placeholder={t('Email')}
+              id="email"
+              name="email"
+              type="email"
+              value={email}
+              onChange={handleInputChange}
+              className="bg-gray-100 w-full p-3 rounded text-sm md:col-start-1 md:col-end-13 placeholder-gray-500 font-serif"
+              placeholder={t('Email')}
             />
 
             <input
-                id="subject"
-                name="subject"
-                type="text"
-                value={subject}
-                onChange={handleInputChange}
-                className="bg-gray-100 w-full p-3 rounded text-sm md:col-start-1 md:col-end-13 placeholder-gray-500 font-serif"
-                placeholder={t('Subject')}
+              id="subject"
+              name="subject"
+              type="text"
+              value={subject}
+              onChange={handleInputChange}
+              className="bg-gray-100 w-full p-3 rounded text-sm md:col-start-1 md:col-end-13 placeholder-gray-500 font-serif"
+              placeholder={t('Subject')}
             />
 
             <textarea
-                id="comment"
-                name="comment"
-                value={comment}
-                onChange={handleInputChange}
-                className="bg-gray-100 w-full p-3 rounded text-sm md:col-start-1 md:col-end-13 md:h-24 resize-none placeholder-gray-500 font-serif"
-                placeholder={t('Description')}
+              d="comment"
+              name="comment"
+              value={comment}
+              onChange={handleInputChange}
+              className="bg-gray-100 w-full p-3 rounded text-sm md:col-start-1 md:col-end-13 md:h-24 resize-none placeholder-gray-500 font-serif"
+              placeholder={t('Description')}i
             >
             </textarea>
 
             <button
-                type="submit"
-                className="bg-red-500 hover:bg-red-600 rounded p-2 text-white md:col-start-1 md:col-end-13 placeholder-gray-500 font-serif"
+              type="submit"
+              className="bg-red-500 hover:bg-red-600 rounded p-2 text-white md:col-start-1 md:col-end-13 placeholder-gray-500 font-serif"
             >
-                {t('Send')}
+              {t('Send')}
             </button>
-        </form>
+          </form>
+        </div>
       </div>
     )
 }
