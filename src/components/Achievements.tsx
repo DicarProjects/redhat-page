448f53336234372ce0c18deac3dc7d9787c84681
DicarProjects <Cardona.DevProjects@gmail.com>
2023-12-05 22:05:53 -0500

    improve red hat in numbers

--- src/components/Achievements.tsx
+++ src/components/Achievements.tsx
@@ -5,30 +5,25 @@ export const Achievements = () => {
     const { t } = useTranslation();
 
     return (
-    <section className="bg-[url('./assets/img/map.png')] h-[500px] mt-10 relative overflow-hidden">
-        <div className="w-full h-full bg-black py-20 opacity-30 absolute">
-        </div>
-        <div className="container mx-auto py-5 md:max-w-[700px] xl:max-w-[1200px] flex justify-center absolute bottom-0 z-50">
-            <h3 className="text-2xl md:text-5xl lg:w-4/12 text-white font-bold">{t('Red Hat in numbers')}</h3>
-        </div>
-        <div className="container mx-auto py-5 md:max-w-[700px] xl:max-w-[1200px] flex justify-center absolute bottom-28  md:-right-32 z-50">
-
-            <div className="flex flex-col gap-5 text-white">
-                <div>
-                    <p className="font-bold text-2xl">10 {t('years')}</p>
-                    <p className="capitalize">{t('programmers experience')}</p>
-                </div>
-                <div>
-                    <p className="font-bold text-2xl">6 {t('projects')}</p>
-                    <p className="capitalize">{t('completed worldwide')}</p>
-                </div>
-                <div>
-                    <p className="font-bold text-2xl">4 {t('countries')}</p>
-                    <p className="capitalize">{t('active operations')}</p>
+        <section className="bg-[url('./assets/img/map.png')] h-[500px] mt-10 relative overflow-hidden">
+            <div className="w-full h-full bg-black py-20 opacity-30 absolute"></div>
+            <div className="container mx-auto py-10 md:max-w-[700px] xl:max-w-[1200px] flex justify-between items-center absolute bottom-0 z-50">
+                <h3 className="text-3xl md:text-5xl lg:w-4/12 text-white font-bold text-center mt-10">{t('Red Hat in numbers')}</h3>
+                <div className="flex flex-col gap-5 text-white text-center md:text-right mt-5">
+                    <div className="mt-1">
+                        <p className="font-bold text-2xl">{t('10 Years')}</p>
+                        <p className="capitalize text-lg">{t('Programmers experience')}</p>
+                    </div>
+                    <div className="mt-1">
+                        <p className="font-bold text-2xl">{t('6 Projects')}</p>
+                        <p className="capitalize text-lg">{t('Completed worldwide')}</p>
+                    </div>
+                    <div className="mt-1">
+                        <p className="font-bold text-2xl">{t('4 Countries')}</p>
+                        <p className="capitalize text-lg">{t('Active operations')}</p>
+                    </div>
                 </div>
             </div>
-
-        </div>
-    </section>
+        </section>
     )
 }
